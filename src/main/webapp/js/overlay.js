4283c1df80022fbfcc7f93be8596534c0d4df5ad
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-05-17 23:41:23 +0200

    added a js to control last row of tiles presentation

--- src/main/webapp/js/overlay.js
+++ src/main/webapp/js/overlay.js
@@ -2,3 +2,29 @@
 function overlay_menu(){
     document.getElementById('nav').checked = false;
 }
+
+/* Adapt Last Row of Tiles to Better Alignment */
+function adaptMarginLastTile(){
+    let tiles =document.getElementsByClassName('tile');
+    let container = document.getElementsByClassName('main_home')[0];
+    //If elements are mod3 == 2
+    //Adapt last element margin to = 3 * marginLeft of first element + 1 tile width
+    if(tiles.length > 0 && tiles.length % 3 === 2){
+        console.log("Cont:" + container.offsetWidth);
+        /*let margin2 = tiles[0].offsetWidth + parseInt(window.getComputedStyle(tiles[0]).marginLeft) * 3;*/
+        let margin = tiles[0].offsetWidth + container.offsetWidth * 2.67/100 * 3;
+        tiles[tiles.length - 1].style.marginRight =  margin + 'px';
+        console.log("Margin: " + margin);
+    } else {
+        Array.from(tiles).forEach(t =>{t.style.removeProperty('margin-right');});
+    }
+
+}
+
+window.addEventListener('load', () =>{
+    adaptMarginLastTile();
+});
+
+window.addEventListener('resize', function(event) {
+    adaptMarginLastTile();
+}, true);
