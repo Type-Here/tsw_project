6a7d78b2fe85e6da9fa3ca469489c5e84f51c13e
Borax-Gargamella <157167477+Borax-Gargamella@users.noreply.github.com>
2024-05-19 12:41:52 +0200

    created newsletter servlet, jsp, css, and js

---
+++ src/main/java/com/unisa/store/tsw_project/controller/NewsletterServlet.java
@@ -0,0 +1,80 @@
+package com.unisa.store.tsw_project.controller;
+
+import com.unisa.store.tsw_project.model.DAO.ProductDAO;
+import com.unisa.store.tsw_project.model.beans.ProductBean;
+import com.unisa.store.tsw_project.other.JSONMetaParser;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "NewsletterServlet", urlPatterns = {"/newsletter"})
+public class NewsletterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ProductDAO productDAO = new ProductDAO();
+
+        try {
+            List<ProductBean> products = getSeasonDiscountGames(request);
+
+            //Retrieve Images for Products from JSON
+            JSONMetaParser parser = new JSONMetaParser();
+            parser.doParseMetaData(products, getServletContext());
+
+            //Add product list to request and Send to JSP for View
+            request.setAttribute("newsletter", products);
+
+            // Inoltra la richiesta a newsletter.jsp
+            request.getRequestDispatcher("/jsp/newsletter.jsp").forward(request, response);
+
+        } catch (SQLException e){
+            throw new RuntimeException(e); /*TODO*/
+        }
+    }
+
+    private List<ProductBean> getSeasonDiscountGames(HttpServletRequest request) throws IOException, SQLException {
+    	List<ProductBean> products = new ArrayList<>();
+        ProductDAO productDAO = new ProductDAO();
+        String season;
+    	int month = java.time.LocalDate.now().getMonthValue();
+    	if(month == 3 ) {
+    		season = "spring";
+    	} else if(month == 6) {
+    		season = "summer";
+    	} else if(month == 9) {
+    		season = "autumn";
+    	} else if(month == 12){
+    		season = "winter";
+    	} else {
+    		season = "noSeason";
+    	}
+
+        request.setAttribute("season", season);
+
+        for(int id : getSeasonDiscountGames(season)){
+            ProductBean p = productDAO.doRetrieveById(id);
+            //Protect from Errors: check if null
+            if(p != null) products.add(p);
+        }
+
+    	return products;
+    }
+
+    private List<Integer> getSeasonDiscountGames(String season) throws IOException {
+
+        JSONMetaParser parser = new JSONMetaParser();
+        List<Integer> discountGames = parser.doParseNewsletter(File.separator + "WEB-INF" +
+                File.separator +"data" + File.separator + season +"-discount.json", getServletContext());
+        return discountGames;
+
+    }
+}
---
+++ src/main/webapp/jsp/newsletter.jsp
@@ -0,0 +1,48 @@
+<%@ page import="java.time.LocalDate" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link type="text/css" rel="stylesheet" href="${pageContext.request.contextPath}/css/style.css" />
+    <link type="text/css" rel="stylesheet" href="${pageContext.request.contextPath}/css/newsletter.css" />
+    <link type="text/css" rel="stylesheet" media="screen and (max-width: 700px)" href="${pageContext.request.contextPath}/css/medium-size.css" />
+    <link type="text/css" rel="stylesheet" media="screen and (min-width: 700px)" href="${pageContext.request.contextPath}/css/widescreen.css" />
+    <link type="text/css" rel="stylesheet" media="screen and (max-width: 450px)" href="${pageContext.request.contextPath}/css/small.css" />
+    <link rel="icon" type="image/x-icon" href="${pageContext.request.contextPath}/img/favicon.ico">
+    <script src="${pageContext.request.contextPath}/js/overlay.js"></script>
+    <script src="${pageContext.request.contextPath}/js/newsletter.js"></script>
+    <title>Retrogamer: Newsletter</title>
+</head>
+<body class="body_def">
+
+<%@include file="/WEB-INF/include/header-standard.jsp"%>
+
+<div class="main_home">
+
+    <c:choose>
+        <c:when test="${season == 'spring'}">
+            <%@include file="/WEB-INF/include/newsletter-spring.jsp" %>
+        </c:when>
+        <c:when test="${season == 'summer'}">
+            <%@include file="/WEB-INF/include/newsletter-summer.jsp" %>
+        </c:when>
+        <c:when test="${season == 'autumn'}">
+            <%@include file="/WEB-INF/include/newsletter-autumn.jsp" %>
+        </c:when>
+        <c:when test="${season == 'winter'}">
+            <%@include file="/WEB-INF/include/newsletter-winter.jsp" %>
+        </c:when>
+        <c:otherwise>
+            <%@include file="/WEB-INF/include/newsletter-normal.jsp" %>
+        </c:otherwise>
+    </c:choose>
+
+</div>
+
+<%@include file="/WEB-INF/include/footer.jsp"%>
+
+</body>
+</html>
