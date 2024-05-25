16170b779216fbea83f4edaedc84bfae667dd242
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-05-25 21:29:20 +0200

    add-product.js: improved var names and code organization

--- src/main/webapp/js/add-product.js
+++ src/main/webapp/js/add-product.js
@@ -4,27 +4,28 @@ function isEmpty(value) {
 }
 
 /* INPUT Physic 0, Digital 1 */
-const typeProd= document.getElementsByClassName('type-prod');
+const typeAddProd= document.getElementsByClassName('type-prod');
 /* NB DIV with checkbox and quantity together*/
-const conditions = document.getElementsByClassName('condition-input');
+const conditionsAdd = document.getElementsByClassName('condition-input');
 /*Get Categories INPUT */
-const categories = document.getElementsByClassName('category-input')
+const categoriesAdd = document.getElementsByClassName('category-input');
 
 /* Physic Type */
-typeProd[0].addEventListener('click', () =>{
-    Array.from(conditions).forEach(cond =>{
+typeAddProd[0].addEventListener('click', () =>{
+    Array.from(conditionsAdd).forEach(cond =>{
         Array.from(cond.children).forEach( c =>{ c.children[0].checked = false; c.children[0].disabled = false});
     });
-    Array.from(conditions[0].children).forEach( c =>{ c.children[0].disabled = true});
+    Array.from(conditionsAdd[0].children).forEach( c =>{ c.children[0].disabled = true});
 });
 
 /* Digital Type */
-typeProd[1].addEventListener('click', () =>{
-    Array.from(conditions).forEach(cond =>{
+typeAddProd[1].addEventListener('click', () =>{
+    Array.from(conditionsAdd).forEach(cond =>{
         Array.from(cond.children).forEach( c =>{ c.children[0].disabled = true});
     });
-    Array.from(conditions[0].children).forEach( c =>{ c.children[0].checked = false; c.children[0].disabled = false});
+    Array.from(conditionsAdd[0].children).forEach( c =>{ c.children[0].checked = false; c.children[0].disabled = false});
 });
+
 
 /* ----------- VALIDATE FORM ON SUBMIT ---------- */
 document.getElementById('add-form').addEventListener("submit", function(e){
@@ -32,18 +33,21 @@ document.getElementById('add-form').addEventListener("submit", function(e){
     //Stop Submit
     e.preventDefault();
 
-    /* Check for:
-     * Checked at least 1 Category
-     * Checked at least 1 Condition + Set its Quantity
-     */
-    let isValid = Array.from(categories).find( cat =>{ return cat.checked === true }) !== undefined
-                                && Array.from(conditions).find(cond => {
-                                    return cond.children[0].children[0].checked === true //Condition
-                                        && (cond.children[1] === undefined || //Bypass if Digital
-                                            !isEmpty(cond.children[1].children[0].value)) //Quantity
-                                    })  !== undefined;
-
-    if(isValid){
+    if(validationForm(categoriesAdd, conditionsAdd)){
         document.getElementById('add-form').submit();
     }
 });
+
+
+/* Check for:
+     * Checked at least 1 Category
+     * Checked at least 1 Condition + Set its Quantity
+     */
+function validationForm(categories, conditions){
+    return Array.from(categories).find( cat =>{ return cat.checked === true }) !== undefined
+    && Array.from(conditions).find(cond => {
+        return cond.children[0].children[0].checked === true //Condition
+            && (cond.children[1] === undefined || //Bypass if Digital
+                !isEmpty(cond.children[1].children[0].value)) //Quantity
+    })  !== undefined;
+}
