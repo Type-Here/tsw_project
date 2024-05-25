b5737782640e100e5fa70ee261aadaa5752b14cd
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-05-25 21:34:12 +0200

    ProdManagerServlet: now has update and delete product options and methods

--- src/main/java/com/unisa/store/tsw_project/controller/admin/ProdManagerServlet.java
+++ src/main/java/com/unisa/store/tsw_project/controller/admin/ProdManagerServlet.java
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken;
 import com.unisa.store.tsw_project.model.DAO.ProductDAO;
 import com.unisa.store.tsw_project.model.beans.ProductBean;
 import com.unisa.store.tsw_project.other.DataValidator;
+import com.unisa.store.tsw_project.other.JSONMetaParser;
 import com.unisa.store.tsw_project.other.exceptions.InvalidParameterException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,10 +15,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @WebServlet(name = "AdminProdManager", urlPatterns = "/WEB-INF/admin/prod-manager")
 public class ProdManagerServlet extends HttpServlet {
@@ -41,7 +42,7 @@ public class ProdManagerServlet extends HttpServlet {
 
         try {
             switch (ask.get()) {
-                case "modifyProd":
+                case "modifyProd": //Send Product data to View to be modified
                     Optional<String> id = Optional.ofNullable(req.getParameter("id"));
                     if(id.isEmpty() || !validator.validatePattern(id.get(), DataValidator.PatternType.Int)) {
                         throw new InvalidParameterException("id");
@@ -77,6 +78,17 @@ public class ProdManagerServlet extends HttpServlet {
                         throw new InvalidParameterException("search");
                     }
                     doRetrieveByName(search.get(), resp);
+                    break;
+
+                case "deleteProd":
+                    doDeleteProd(req, resp);
+                    break;
+
+                case "saveModProd":
+                    for(String p : req.getParameterMap().keySet()){
+                        System.out.println(p + " : " + Arrays.toString(req.getParameterMap().get(p)));
+                    }
+                    doUpdateProd(req, resp);
                     break;
             }
         } catch (Exception e){
@@ -136,6 +148,96 @@ public class ProdManagerServlet extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         resp.getWriter().print(json);
 
+    }
+
+
+    private void doUpdateProd(HttpServletRequest req, HttpServletResponse resp) throws IOException, SQLException {
+        DataValidator validator = new DataValidator();
+        ProductDAO productDAO = new ProductDAO();
+        Optional<String> id = Optional.ofNullable(req.getParameter("id_prod"));
+        if(id.isEmpty() || !validator.validatePattern(id.get(), DataValidator.PatternType.Int)) {
+            throw new InvalidParameterException("id");
+        }
+
+        ProductBean p = productDAO.doRetrieveById(Integer.parseInt(id.get()));
+
+        JSONMetaParser parser = new JSONMetaParser();
+        parser.doParseMetaData(p, getServletContext());
+
+        Map<String, String[]> parameters = req.getParameterMap();
+
+        /* Each validate throws Error if Values are not valid */
+        AddProdServlet.validateCategoriesAndSet(p, req.getParameterValues("category"), getServletContext());
+        AddProdServlet.validateConditionsAndSet(p, req.getParameterValues("condition"), req.getParameterValues("quantity"));
+        AddProdServlet.validateParametersAndSetProd(p, parameters);
+
+        try{
+            productDAO.doUpdate(p);
+            resp.sendError(HttpServletResponse.SC_OK, "Product updated successfully");
+        } catch (RuntimeException e){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    private void doDeleteProd(HttpServletRequest request, HttpServletResponse resp) throws IOException, SQLException {
+        Optional<String> id = Optional.ofNullable(request.getParameter("id"));
+        DataValidator validator = new DataValidator();
+
+        if(id.isEmpty() || !validator.validatePattern(id.get(), DataValidator.PatternType.Int)) {
+            throw new InvalidParameterException("id");
+        }
+        String message = "OK";
+        ProductDAO productDAO = new ProductDAO();
+        ProductBean p = productDAO.doRetrieveById(Integer.parseInt(id.get()));
+
+        try {
+            JSONMetaParser parser = new JSONMetaParser();
+            parser.doParseMetaData(p, getServletContext());
+        } catch (Exception e){
+            message = " Unable to delete metadata json;";
+        }
+
+        if(deleteMetaData(p)) message += " Unable to delete metadata";
+        try {
+            productDAO.doRemoveById(p);
+        } catch (SQLException e){
+            message = " Unable to delete product: " + e.getMessage();
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, message);
+            return;
+        }
+        //No actual error SEND OK
+        message += " Successfully deleted";
+        resp.sendError(HttpServletResponse.SC_OK, message);
+    }
+
+    private boolean deleteMetaData(ProductBean prod) {
+        if(prod == null) return false;
+        try {
+            String path = prod.getMetaData().getPath();
+            String front = prod.getMetaData().getFront();
+            List<String> gallery = prod.getMetaData().getGallery();
+
+            String pathCorrected;
+            //MacOS Parsing of getResource gives problems with WhiteSpaces: replace %20 with a whitespace
+            if (System.getProperty("os.name").contains("mac") || System.getProperty("os.name").contains("Mac")) {
+                pathCorrected = getServletContext().getResource(path + front).getFile().replaceAll("%20", " ");
+            } else {
+                pathCorrected = getServletContext().getResource(path + front).getFile();
+            }
+
+            if (!Files.deleteIfExists(Path.of(pathCorrected))) return false;
+
+            for (String s : gallery) {
+                pathCorrected = getServletContext().getResource(path + s).getFile().replaceAll("%20", " ");
+                if (!Files.deleteIfExists(Path.of(pathCorrected))) return false;
+            }
+
+            String jsonPath = getServletContext().getRealPath("metadata/" + prod.getPlatform() + '/' + prod.getMetadataPath()).replaceAll("%20", " ");
+
+            return Files.deleteIfExists(Path.of(jsonPath));
+        } catch (RuntimeException | IOException e){
+            return false;
+        }
     }
 
 }
