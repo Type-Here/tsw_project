901a392a473f588c66b221cdc010b91932c555d5
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-06-08 01:09:01 +0200

    CartDAO: now retrieve and save new Cart fields. Added: doRetrieveActive to retrieve active cart. Minor Improvements

--- src/main/java/com/unisa/store/tsw_project/model/DAO/CartDAO.java
+++ src/main/java/com/unisa/store/tsw_project/model/DAO/CartDAO.java
@@ -8,21 +8,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CartDAO {
-
+    /**
+     * Retrieve a Cart by its ID
+     * @param id_cart CartID to retrieve
+     * @return a CartBean or Null if not found
+     */
     public CartBean doRetrieveById(int id_cart){
         try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps = con.prepareStatement("SELECT * FROM cart WHERE id_cart = ?");
             ps.setInt(1, id_cart);
             ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                CartBean c = new CartBean();
-                c.setId_cart(rs.getInt(1));
-                c.setTotal(rs.getBigDecimal(2));
-                c.setId_client(rs.getInt(3));
-                c.setDiscount_code(rs.getString(4));
-                return c;
-            }
-            return null;
+            return populateBean(rs);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -33,15 +29,7 @@ public class CartDAO {
             PreparedStatement ps = con.prepareStatement("SELECT * FROM cart WHERE id_client = ?");
             ps.setInt(1, id_client);
             ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                CartBean c = new CartBean();
-                c.setId_cart(rs.getInt(1));
-                c.setTotal(rs.getBigDecimal(2));
-                c.setId_client(rs.getInt(3));
-                c.setDiscount_code(rs.getString(4));
-                return c;
-            }
-            return null;
+            return populateBean(rs);
         }
     }
 
@@ -54,11 +42,14 @@ public class CartDAO {
     public void doSave(CartBean cart) throws SQLException {
         try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps = con.prepareStatement("INSERT INTO cart " +
-                    "(total, id_client, discount_code) VALUES(?, ?, ?)",
+                    "(total, id_client, discount_code, active) VALUES(?, ?, ?, ?)",
                     PreparedStatement.RETURN_GENERATED_KEYS);
+
             ps.setBigDecimal(1, cart.getTotal());
             ps.setInt(2, cart.getId_client());
             ps.setString(3, cart.getDiscount_code());
+            ps.setObject(4, cart.getActive());
+
             if (ps.executeUpdate() != 1) {
                 throw new RuntimeException("INSERT error.");
             }
@@ -68,17 +59,63 @@ public class CartDAO {
         }
     }
 
+    /**
+     * Update Cart Bean by CartID
+     * @param cart with the data to be updated. It needs to have a valid cartID
+     * @throws SQLException if update fails
+     */
     public void doUpdate(CartBean cart) throws SQLException {
         try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps = con.prepareStatement("UPDATE cart " +
-                    "SET total=?, id_client=?, discount_code=? WHERE id_cart=?");
+                    "SET total=?, id_client=?, discount_code=?, active=? WHERE id_cart=?");
             ps.setBigDecimal(1, cart.getTotal());
             ps.setInt(2, cart.getId_client());
             ps.setString(3, cart.getDiscount_code());
-            ps.setInt(4, cart.getId_cart());
+            ps.setObject(4, cart.getActive());
+            ps.setInt(5, cart.getId_cart());
             if (ps.executeUpdate() != 1) {
                 throw new RuntimeException("UPDATE error.");
             }
         }
     }
+
+    /**
+     * Retrieve Active Cart for a User by his ID. <br />
+     * Active Cart is a Cart that is saved but not ordered yet by the user.
+     * @param id_client User ID to retrieve data from
+     * @return CartBean if retrieved, else Null
+     * @throws SQLException if query fails
+     */
+    public CartBean doRetrieveActive(int id_client) throws SQLException {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(" SELECT * FROM cart WHERE id_client=? AND active = true");
+            ps.setInt(1, id_client);
+            ResultSet rs = ps.executeQuery();
+            return populateBean(rs);
+        }
+    }
+
+    /* ----------------------------- PRIVATE METHODS ------------------------------- */
+
+    /**
+     * Set a CartBean with a ResultSet Data
+     * @param rs ResultSet
+     * @return a CartBean or Null if rs is empty
+     * @throws SQLException if getter/setter from RS fails
+     */
+    private CartBean populateBean(ResultSet rs) throws SQLException {
+        if (rs.next()) {
+            CartBean c = new CartBean();
+            c.setId_cart(rs.getInt(1));
+            c.setTotal(rs.getBigDecimal(2));
+            c.setId_client(rs.getInt(3));
+            c.setDiscount_code(rs.getString(4));
+            c.setActive((Boolean) rs.getObject(5));
+            return c;
+        }
+        return null;
+    }
+
+
+
 }
