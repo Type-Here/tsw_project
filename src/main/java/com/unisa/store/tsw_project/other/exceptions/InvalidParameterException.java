4111e1542547fad9984d2a9b07cc6b1d4dc9a489
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-06-21 17:53:52 +0200

    InvalidParameterException: Optimized exception messages

--- src/main/java/com/unisa/store/tsw_project/other/exceptions/InvalidParameterException.java
+++ src/main/java/com/unisa/store/tsw_project/other/exceptions/InvalidParameterException.java
@@ -2,10 +2,10 @@ package com.unisa.store.tsw_project.other.exceptions;
 
 public class InvalidParameterException extends RuntimeException{
     public InvalidParameterException() {
-        super("Invalid Parameter: ");
+        super("Invalid Parameter. ");
     }
 
     public InvalidParameterException(String message) {
-        super("Invalid Parameter: " + message);
+        super("E: " + message);
     }
 }
