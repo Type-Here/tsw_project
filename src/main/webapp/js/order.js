9e723b7933bbcc9e5220ba157a223ea0ee1af05c
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-06-21 03:15:07 +0200

    Order JS: listeners, validation and Ajax for Add Address form

--- src/main/webapp/js/order.js
+++ src/main/webapp/js/order.js
@@ -24,12 +24,15 @@ async function ajax(message, responseType = 'text'){
  * Address Button Tiles Listeners
  */
 Array.from(document.getElementsByClassName('address-button'))
-                .forEach( function(btn, index, array){
+                .forEach( function(btn){
     if(btn.id === 'add-address-btn') return;
     btn.addEventListener('click', function (){
-        array.forEach( b => b.classList.remove('address-button-selected'));
+
+        //Don't use array bind since elements can change! (i.e. adding a new address)
+        Array.from(document.getElementsByClassName('address-button'))
+            .forEach( b => b.classList.remove('address-button-selected'));
         btn.classList.add('address-button-selected');
-    }.bind(array));
+    });
 });
 
 
@@ -128,3 +131,146 @@ function doPresent(data){
         }
     }, 7);
 }
+
+
+
+/* ============================================== ADD ADDRESS ================================================================= */
+
+/**
+ * Add Address Button Listener: On Click Show Add Address Section
+ */
+document.getElementById('add-address-btn').addEventListener('click', () =>{
+    const section = document.getElementById('add-address');
+    section.classList.remove('general-display-none');
+    section.style.animation = 'Unfold 2s ease-out';
+    section.scrollIntoView({behavior:'smooth'});
+});
+
+
+
+
+/**
+ * Keeps Data in Add Address for Valid for each field (Listeners)
+ */
+Array.from(document.forms.namedItem('address-form').elements).forEach(element =>{
+    element.addEventListener('input', function (e){
+        if(!e.data ||element.type === 'submit') return;
+
+        let pattern;
+        switch (element.id){
+            case 'road-type2':
+            case 'road-name2':
+            case 'city2':
+                pattern = /^[A-Za-zÀ-ÿ' -]+$/;
+                break;
+            case 'road-number2':
+            case 'cap2':
+                pattern = /^[0-9]+$/;
+                break;
+            case 'prov2':
+                pattern = /^[a-zA-Z]$/;
+                break;
+            default:
+                return;
+        }
+        const maxlength = parseInt(this.getAttribute('maxlength'));
+        if(!pattern.test(e.data)) this.value = this.value.slice(0, this.value.length - e.data.length);
+        if(this.value.length > maxlength) this.value = this.value.slice(0, maxlength);
+        if(this.id === 'prov2') this.value = this.value.toUpperCase();
+    });
+});
+
+/**
+ * Form Add Address, Validate and
+ * Send Data AJAX to Add a New Shipping Address for the USer
+ */
+document.forms.namedItem('address-form').addEventListener('submit', async function (e){
+    e.preventDefault();
+    // Info Message Span
+    let span = document.getElementById('info-address-span');
+    if(!span){
+        span = document.createElement('span');
+        span.classList.add('text-center-full-width');
+        span.id = 'info-address-span';
+        this.appendChild(span);
+    }
+
+    try{
+
+        //Validate Data
+        Array.from(this.elements).forEach(element =>{
+            if(element.pattern){
+                let pattern = new RegExp(element.pattern);
+                if(!pattern.test(element.value)) {
+                    throw new Error("Campo non valido: " + element.value);
+                }
+            }
+        });
+
+        //Get Input Values
+        let data = new FormData(this);
+        console.log(data);
+
+        let message = 'section=four';
+        for(const key of data.keys()){
+            message += '&' + key + '=' + data.get(key).trim();
+        }
+
+        //AJAX
+        let response = await fetch('modify-user', {
+            headers:{
+                "Content-type": "application/x-www-form-urlencoded",
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+            method:'POST',
+            body: message
+        });
+        let responseData = await response.json();
+
+        span.classList.add('success-info');
+        span.innerHTML = "Indirizzo Aggiunto con Successo";
+
+        //Create a New Div/Button for the New Address
+        const newDiv = document.getElementsByClassName('address-button-selected')[0].cloneNode(true);
+        newDiv.addEventListener('click', function () {
+            Array.from(document.getElementsByClassName('address-button'))
+                .forEach(b => b.classList.remove('address-button-selected'));
+            this.classList.add('address-button-selected');
+        });
+
+        //Deselect old Address
+        const addressSection = document.getElementById('address');
+        Array.from(addressSection.children).forEach(div => {
+            div.classList.remove('address-button-selected');
+        })
+
+        //Add new Div to Address Section
+        addressSection.appendChild(newDiv);
+
+        //Populate Address with thw new Data
+        const newDivElement = document.getElementsByClassName('address-button-selected')[0];
+        newDivElement.getElementsByTagName('input')[0].value = responseData['id_add'];
+        newDivElement.getElementsByClassName('item-data')[0].innerHTML =
+            '<li>' + responseData['address'] + '</li>' +
+            '<li>' + responseData['city'] + ' ' + responseData['prov'] + '</li>' +
+            '<li>' + responseData['CAP'] + '</li>';
+
+        //Animate re-fold
+        const addSection = document.getElementById('add-address');
+        addSection.style.animation = '';
+
+        setTimeout(() =>{
+            addSection.style.animation = 'Unfold reverse 2s';
+        },50)
+
+        setTimeout(()=>{
+            addSection.classList.add('general-display-none');
+        },4000);
+
+    } catch (e) {
+        span.classList.add('invalid-credentials');
+        span.innerHTML = e;
+        this.appendChild(span);
+    }
+
+});
