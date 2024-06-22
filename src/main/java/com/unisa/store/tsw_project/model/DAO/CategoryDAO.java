dc58ca586468142a280bdf9eb50baa502af1cf5b
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-06-22 03:15:07 +0200

    CategoryDAO: added doUpdate (Full), do DeleteAll by ProductBean methods

--- src/main/java/com/unisa/store/tsw_project/model/DAO/CategoryDAO.java
+++ src/main/java/com/unisa/store/tsw_project/model/DAO/CategoryDAO.java
@@ -33,7 +33,7 @@ public class CategoryDAO {
 
 
 
-    public List<CategoryBean> doRetriveCategoryListByProductId(int id) throws SQLException {
+    public List<CategoryBean> doRetrieveCategoryListByProductId(int id) throws SQLException {
         try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps =
                     con.prepareStatement("SELECT id_cat, typename FROM prod_categories NATURAL JOIN " +
@@ -55,7 +55,7 @@ public class CategoryDAO {
                     con.prepareStatement("INSERT INTO categories (typename)  VALUES (?)");
             ps.setString(1, categoryBean.getTypename());
             if (ps.executeUpdate() != 1) {
-                throw new RuntimeException("INSERT error.");
+                throw new RuntimeException("INSERT Category error.");
             }
         }
     }
@@ -68,11 +68,46 @@ public class CategoryDAO {
                 ps.setInt(1, productBean.getId_prod());
                 ps.setInt(2, categoryBean.getId_cat());
                 if (ps.executeUpdate() != 1) {
-                    throw new RuntimeException("INSERT error.");
+                    throw new RuntimeException("INSERT Category Error.");
                 }
             }
         }
     }
+
+
+    /**
+     * Update Categories <br/>
+     * Use this method to update quantity by an absolute value (I.E. for Admin Update) <br />
+     * Update all Categories in list of a specific product -- DELETE THEN SAVE <br />
+     * @param productBean needs to have SET id_prod before calling this method
+     * @throws SQLException if update fails
+     * @apiNote <b>Bean needs to have SET id_prod before calling this method</p>
+     */
+    synchronized public void doFullUpdateByIDProduct(ProductBean productBean) throws SQLException {
+        //Since we're not sure if some elements are removed or not by Admin:
+        //Remove all Conditions then Set new
+        doDeleteAllByIDProduct(productBean);
+        doSaveProductCategories(productBean);
+    }
+
+
+    /**
+     * Delete all Product Categories in Database
+     * @param productBean product to remove all categories from
+     * @throws SQLException if query fails
+     */
+    synchronized public void doDeleteAllByIDProduct(ProductBean productBean) throws SQLException {
+        try (Connection con = ConPool.getConnection()) {
+            if(productBean.getId_prod() <= 0) throw new RuntimeException("No ID Product Set in Update Categories");
+            PreparedStatement ps = con.prepareStatement("DELETE FROM prod_categories WHERE id_prod = ?");
+            ps.setInt(1, productBean.getId_prod());
+            ps.executeUpdate();
+        }
+    }
+
+
+
+
 
 
     /* ------------------ PRIVATE METHODS ------------------- */
